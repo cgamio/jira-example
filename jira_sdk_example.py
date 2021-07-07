5777eb60fdb40652318329e33eb2588735ce686f
Chris Gamio <cgamio@cap-rx.com>
2021-07-07 09:30:49 -0400

    simple example using the jira python sdk

---
+++ jira_sdk_example.py
@@ -0,0 +1,8 @@
+from jira import JIRA
+import os
+
+if __name__ == '__main__':
+    jira_host = os.environ["JIRA_HOST"]
+    jira_user = os.environ["JIRA_USER"]
+    jira_token = os.environ["JIRA_TOKEN"]
+    jira = JIRA(f"https://{jira_host}", basic_auth=(jira_user, jira_token))
