b7cd9a14c16f300455cbd10cfab511608f6f37d0
Chris Gamio <cgamio@cap-rx.com>
2021-07-07 09:59:00 -0400

    Simple get jira issue example via SDK

--- jira_sdk_example.py
+++ jira_sdk_example.py
@@ -6,3 +6,8 @@ if __name__ == '__main__':
     jira_user = os.environ["JIRA_USER"]
     jira_token = os.environ["JIRA_TOKEN"]
     jira = JIRA(f"https://{jira_host}", basic_auth=(jira_user, jira_token))
+
+
+    # Simple 'get jira issue' example
+    # issue = jira.issue('ABC-1234')
+    # print(issue)
